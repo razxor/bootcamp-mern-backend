ad8c44d6732d254c7c5740bd21380e3a4ba3cd5b
Raz Ahamed <raz.abcoder@gmail.com>
2024-10-26 10:24:22 +0600

    deployment test 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const salt = 10;
 const port = process.env.PORT || 3000
 
@@ -13,15 +13,6 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Welcome to my site');
 })
-
-
-// app.get('/api/courses/:id', (req, res) => {
-//     let id = req.params.id
-//     let data = courses.find((item) => {
-//         return item.course_id == id
-//     })
-//     res.send(data);
-// })
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
