5999ef86b38bc9e0a0e5e7658119cdc8a88be640
Raz Ahamed <raz.abcoder@gmail.com>
2024-10-26 15:50:55 +0600

    issue fix

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ const client = new MongoClient(uri, {
     }
 });
 
-router.get('/route', async (req, res) => {    
-    res.send('API Route working')
-})
-
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
